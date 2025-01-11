fc0c3783927744514fea0f651b2812f4b80181e8
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-01-11 10:19:46 -0500

    update

--- IaC/main.tf
+++ IaC/main.tf
@@ -127,6 +127,15 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   cidr_ipv4 = "0.0.0.0/0"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  description = "Allow HTTP IPv4"
+  from_port = 80
+  ip_protocol = "tcp"
+  to_port = 80
+  cidr_ipv4 = "0.0.0.0/0"
+}
+
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   security_group_id = aws_security_group.allow_tls.id
   description = "Allow all traffic IPv6"
