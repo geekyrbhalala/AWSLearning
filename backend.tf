7d793cc045b1867488fe92a14720493db03102f4
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-01-11 15:07:40 -0500

    update

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-for-demo-vpc"
+    key    = "tf/terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-locks"
+    encrypt = true
+  }
+}
