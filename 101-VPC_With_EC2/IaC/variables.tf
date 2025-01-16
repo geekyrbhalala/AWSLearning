2c797720c813a82e231db232ddb41087d14dbafb
ravibhalala11795 <geekyrbhalala@gmail.com>
2025-01-16 16:18:11 -0500

    update

--- 101-VPC_With_EC2/IaC/variables.tf
+++ 101-VPC_With_EC2/IaC/variables.tf
@@ -20,19 +20,23 @@ variable "instance_type" {
 variable "vpc_name" {
   type = string
   description = "The AWS VPC name"
+  default = "demo-vpc"
 }
 
 variable "main_cidr_block" {
   type = string
   description = "The CIDR block of main vpc. i.e. 10.0.0.0/16"
+  default = "11.0.0.0/16"
 }
 
 variable "subnet_cidr_block" {
   type = list(string)
   description = "List of CIDR block of subnet. i.e. [10.0.1.0/24,10.0.2.0/24]"
+  default = ["11.0.1.0/24", "11.0.2.0/24", "11.0.3.0/24", "11.0.4.0/24"]
 }
 
 variable "availability_zones" {
   type = list(string)
   description = "List of availability zones. i.e. [us-east-1a, us-east-1b]"
+  default = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
 }
